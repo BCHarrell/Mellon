e1b8731ec54e9e7886c1d1fb179a4debcf2f61db
brenther <one_into_god@yahoo.com>
2017-03-23 16:16:20 -0400

    Update LoginPage.java
    Changed the class to extend VBox, BorderPane wasn't necessary.  Added method to go to the new sign up page.  Commented out the bind line -- I don't fully understand that, but it doesn't seem necessary

--- src/mellon/LoginPage.java
+++ src/mellon/LoginPage.java
@@ -14,9 +14,9 @@ import javafx.scene.text.Text;
  *
  * @author Brent H.
  */
-public class LoginPage extends BorderPane {
+public class LoginPage extends VBox {
 
-    MellonGUI parent;
+    MellonFramework parent;
     ImageView logo = new ImageView(new Image(getClass()
             .getResourceAsStream("/resources/templogo.png")));
 
@@ -24,12 +24,13 @@ public class LoginPage extends BorderPane {
         parent = p;
         addItems();
     }
-
-    ;
     
     private void addItems() {
+        this.setMaxSize(350, 450);
+        this.setAlignment(CENTER);
+        this.setSpacing(45);
+        
         VBox vb = new VBox();
-        vb.setMaxSize(350, 400);
         vb.setAlignment(CENTER);
         vb.setSpacing(15);
         TextField username = new TextField();
@@ -38,21 +39,23 @@ public class LoginPage extends BorderPane {
         PasswordField password = new PasswordField();
         password.setMaxWidth(300);
         password.setPromptText("Password");
-        Text scenetitle = new Text("Welcome to Mellon, Please login to continue");
+        
         HBox hb = new HBox();
         hb.setAlignment(CENTER);
         hb.setSpacing(15);
         Button login = new Button("Log In");
         Button signUp = new Button("Sign Up");
+        
         hb.getChildren().addAll(login, signUp);
-
-        vb.getChildren().addAll(logo,scenetitle,username, password, hb);
-        this.setCenter(vb);
-      
-        vb.managedProperty().bind(login.visibleProperty());
-        vb.setVisible(true);
-
-
+        vb.getChildren().addAll(username, password, hb);
+        this.getChildren().addAll(logo, vb);
+        
+        /*I don't think these two lines are necessary but I left them
+        in case I'm missing something.  GUI should run without this*/
+        //vb.managedProperty().bind(login.visibleProperty());
+        //vb.setVisible(true);
+        
+        //Event Listeners
         // Doing this just to test...
         login.setOnAction(e -> {
             try {
@@ -64,6 +67,10 @@ public class LoginPage extends BorderPane {
 
             }
 
+        });
+        //Goes to sign up page
+        signUp.setOnAction(e -> {
+            parent.getScene().setRoot(new SignUpPage(parent, this));
         });
         
 // Testing DBConnect and creating MellonUser object " uncomment to perform test"
