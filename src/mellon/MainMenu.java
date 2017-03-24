9bc13719d6002cc93ba6b39fcb2f700fcee2f8e3
brenther <one_into_god@yahoo.com>
2017-03-24 17:06:18 -0400

    Update MainMenu.java
    Now holds the main menu.  Very basic, no design elements

--- src/mellon/MainMenu.java
+++ src/mellon/MainMenu.java
@@ -2,25 +2,48 @@
 package mellon;
 
 
-import javafx.scene.Parent;
+import javafx.geometry.Pos;
 import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 
 /**
  *
  * @author Brent H.
  */
-public class MainMenu extends BorderPane{
+public class MainMenu extends BorderPane {
     MellonFramework parent;
     
-    public MainMenu(MellonFramework p){
+    public MainMenu(MellonFramework p) {
         parent = p;
         addItems();
     };
     
-    private void addItems(){
-        //BorderPane border = new BorderPane();
-        Button btn = new Button("Change to login");
-        this.setCenter(btn);
+    private void addItems() {
+        Button create = new Button("Create a Password");
+        Button retrieve = new Button("Retrieve a Password");
+        
+        HBox hb = new HBox();
+        hb.setAlignment(Pos.CENTER);
+        hb.setSpacing(15);
+        hb.getChildren().addAll(create, retrieve);
+        
+        this.setCenter(hb);
+        
+        //Temporary solution, will probably use a StackPane and won't need
+        //these on every menu.  Will also use icons
+        Button settings = new Button("Settings");
+        Button help = new Button("Help");
+        
+        HBox settingsBar = new HBox();
+        settingsBar.setAlignment(Pos.BOTTOM_RIGHT);
+        settingsBar.setSpacing(15);
+        settingsBar.getChildren().addAll(settings, help);
+        
+        AnchorPane anch = new AnchorPane();
+        anch.setBottomAnchor(settingsBar, 8.0);
+        anch.setRightAnchor(settingsBar, 5.0);
+        this.setBottom(settingsBar);
     }
 }
